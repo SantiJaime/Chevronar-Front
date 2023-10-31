295e80e6f59870cb058d61f255d382687b032038
SantiJaime <santij03@gmail.com>
2023-10-31 12:40:29 -0300

    añado contact

--- src/routes/RoutesView.jsx
+++ src/routes/RoutesView.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Homepage from '../pages/Homepage'
-import Errorpage from '../pages/Errorpage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+import Errorpage from "../pages/Errorpage";
+import Contact from "../pages/Contact";
 
 const RoutesView = () => {
   return (
     <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='*' element={<Errorpage/>}/>
-        <Route/>
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="*" element={<Errorpage />} />
+      <Route />
     </Routes>
-  )
-}
+  );
+};
 
-export default RoutesView
+export default RoutesView;
