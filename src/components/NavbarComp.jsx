226cf7bce84b9316fadea076b2f89df0c52f4b77
SantiJaime <santij03@gmail.com>
2023-11-11 18:19:24 -0300

    añado login de usuarios y navbar acorde a c/u

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const NavbarComp = () => {
+  const navigate = useNavigate();
   const [toggle, setToggle] = useState(false);
 
   const handleToggleClick = () => setToggle(!toggle);
 
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const role = JSON.parse(sessionStorage.getItem("role"));
+
+  const logOut = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("role");
+    sessionStorage.removeItem("idUser");
+
+    navigate("/");
+  };
   return (
     <Navbar collapseOnSelect expand="lg" fixed="top">
       <Container fluid>
@@ -34,28 +46,82 @@ const NavbarComp = () => {
             className={`bi ${toggle ? "bi-x" : "bi-list"} text-white fs-1`}
           ></i>
         </button>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <hr className="displayNone text-white" />
-          <Nav className="ms-3 me-auto">
-            <NavLink to="/" className="whiteNavLink margenNavLinks">
-              Inicio
-            </NavLink>
-            <NavLink to="/products" className="whiteNavLink margenNavLinks">
-              Productos
-            </NavLink>
-            <NavLink to="/contact" className="whiteNavLink margenNavLinks">
-              Contacto
-            </NavLink>
-          </Nav>
-          <Nav className="ms-3">
-            <NavLink to="/login" className="whiteNavLink margenNavLinks">
-              Iniciar sesión
-            </NavLink>
-            <NavLink to="/register" className="whiteNavLink margenNavLinks">
-              Registrarse
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
+        {token && role === "user" ? (
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <hr className="displayNone text-white" />
+            <Nav className="ms-3 me-auto">
+              <NavLink to="/" className="whiteNavLink margenNavLinks">
+                Inicio
+              </NavLink>
+              <NavLink to="/products" className="whiteNavLink margenNavLinks">
+                Productos
+              </NavLink>
+              <NavLink to="/contact" className="whiteNavLink margenNavLinks">
+                Contacto
+              </NavLink>
+            </Nav>
+            <Nav className="ms-3">
+              <NavLink to="/login" className="whiteNavLink margenNavLinks">
+              <i className="bi bi-cart"></i> Mi carrito
+              </NavLink>
+              <button
+                onClick={logOut}
+                className="whiteNavLink margenNavLinks bg-transparent border-0"
+              >
+                <i className="bi bi-door-open-fill"></i> Cerrar sesión
+              </button>
+            </Nav>
+          </Navbar.Collapse>
+        ) : token && role === "admin" ? (
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <hr className="displayNone text-white" />
+            <Nav className="ms-3 me-auto">
+              <NavLink to="/" className="whiteNavLink margenNavLinks">
+                Inicio
+              </NavLink>
+              <NavLink to="/products" className="whiteNavLink margenNavLinks">
+                Productos
+              </NavLink>
+              <NavLink to="/contact" className="whiteNavLink margenNavLinks">
+                Contacto
+              </NavLink>
+            </Nav>
+            <Nav className="ms-3">
+              <NavLink to="/login" className="whiteNavLink margenNavLinks">
+                <i className="bi bi-person-fill-gear"></i> Administrador
+              </NavLink>
+              <button
+                onClick={logOut}
+                className="whiteNavLink margenNavLinks bg-transparent border-0"
+              >
+                <i className="bi bi-door-open-fill"></i> Cerrar sesión
+              </button>
+            </Nav>
+          </Navbar.Collapse>
+        ) : (
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <hr className="displayNone text-white" />
+            <Nav className="ms-3 me-auto">
+              <NavLink to="/" className="whiteNavLink margenNavLinks">
+                Inicio
+              </NavLink>
+              <NavLink to="/products" className="whiteNavLink margenNavLinks">
+                Productos
+              </NavLink>
+              <NavLink to="/contact" className="whiteNavLink margenNavLinks">
+                Contacto
+              </NavLink>
+            </Nav>
+            <Nav className="ms-3">
+              <NavLink to="/login" className="whiteNavLink margenNavLinks">
+                Iniciar sesión
+              </NavLink>
+              <NavLink to="/register" className="whiteNavLink margenNavLinks">
+                Registrarse
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        )}
       </Container>
     </Navbar>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,84 +1,123 @@
-import React from 'react'
+import React from "react";
 import { Formik } from "formik";
 import { Button, Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { errorLoginSchema } from "../utils/validationSchemas";
 import InputGroup from "react-bootstrap/InputGroup";
 import { Link, useNavigate } from "react-router-dom";
-// import clientAxios, { config } from "../utils/axiosClient";
-// import Swal from "sweetalert2";
+import clientAxios, { config } from "../utils/axiosClient";
+import Swal from "sweetalert2";
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const loginUser = async (values) => {
+    try {
+      const res = await clientAxios.post(
+        "/usuarios/login",
+        {
+          email: values.email,
+          pass: values.pass,
+        },
+        config
+      );
+      if (res?.data?.token) {
+        sessionStorage.setItem("token", JSON.stringify(res.data.token));
+        sessionStorage.setItem(
+          "idUser",
+          JSON.stringify(res.data.userExist._id)
+        );
+        sessionStorage.setItem("role", JSON.stringify(res.data.userExist.role));
+        res.data?.userExist?.role === "user"
+          ? navigate("/")
+          : navigate("/admin");
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "¡Oh no!",
+          text: "Usuario y/o contraseña incorrectos",
+          timer: 2000,
+          showConfirmButton: false,
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "¡Al parecer hubo un error!",
+        text: error.response.data.msg,
+        timer: 2000,
+        showConfirmButton: false,
+      });
+    }
+  };
+
   return (
     <Container className="my-5 d-flex justify-content-center text-white">
-    <Formik
-      initialValues={{
-        email: "",
-        pass: "",
-      }}
-      validationSchema={errorLoginSchema}
-      onSubmit={(values) => ingresoCuenta(values)}
-    >
-      {({ values, errors, touched, handleChange, handleSubmit }) => (
-        <Form className="fondo p-3 w-75 rounded-3 sombra">
-          <h3>Ingresá a tu cuenta</h3>
-          <hr />
-          <Form.Group className="mb-3" controlId="emailId">
-            <Form.Label>Correo electrónico</Form.Label>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="groupEmail">
-                <i className="bi bi-envelope-at-fill"></i>
-              </InputGroup.Text>
-              <Form.Control
-                placeholder="name@example.com"
-                type="email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                className={errors.email && touched.email && "is-invalid"}
-              />
-            </InputGroup>
-            <small className="text-danger">
-              {errors.email && touched.email && errors.email}
-            </small>
-          </Form.Group>
+      <Formik
+        initialValues={{
+          email: "",
+          pass: "",
+        }}
+        validationSchema={errorLoginSchema}
+        onSubmit={(values) => loginUser(values)}
+      >
+        {({ values, errors, touched, handleChange, handleSubmit }) => (
+          <Form className="fondo p-3 w-75 rounded-3 sombra">
+            <h3>Ingresá a tu cuenta</h3>
+            <hr />
+            <Form.Group className="mb-3" controlId="emailId">
+              <Form.Label>Correo electrónico</Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text id="groupEmail">
+                  <i className="bi bi-envelope-at-fill"></i>
+                </InputGroup.Text>
+                <Form.Control
+                  placeholder="name@example.com"
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  className={errors.email && touched.email && "is-invalid"}
+                />
+              </InputGroup>
+              <small className="text-danger">
+                {errors.email && touched.email && errors.email}
+              </small>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="passId">
-            <Form.Label>Contraseña</Form.Label>
-            <InputGroup className="mb-3">
-              <InputGroup.Text id="groupPass">
-                <i className="bi bi-key-fill"></i>
-              </InputGroup.Text>
-              <Form.Control
-                placeholder="***********"
-                type="password"
-                name="pass"
-                value={values.pass}
-                onChange={handleChange}
-                className={errors.pass && touched.pass && "is-invalid"}
-              />
-            </InputGroup>
-            <small className="text-danger">
-              {errors.pass && touched.pass && errors.pass}
-            </small>
-          </Form.Group>
-          <hr />
-          <div className="d-flex justify-content-between">
-            <Link to={"/register"} className="linkFooter">
-              ¿Aún no tienes cuenta? Registrate aquí
-            </Link>
-            <Button
-              variant='light'
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Iniciar sesión
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </Container>
-  )
-}
+            <Form.Group className="mb-3" controlId="passId">
+              <Form.Label>Contraseña</Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text id="groupPass">
+                  <i className="bi bi-key-fill"></i>
+                </InputGroup.Text>
+                <Form.Control
+                  placeholder="***********"
+                  type="password"
+                  name="pass"
+                  value={values.pass}
+                  onChange={handleChange}
+                  className={errors.pass && touched.pass && "is-invalid"}
+                />
+              </InputGroup>
+              <small className="text-danger">
+                {errors.pass && touched.pass && errors.pass}
+              </small>
+            </Form.Group>
+            <hr />
+            <div className="d-flex justify-content-between">
+              <Link to={"/register"} className="linkFooter">
+                ¿Aún no tienes cuenta? Registrate aquí
+              </Link>
+              <Button variant="light" type="submit" onClick={handleSubmit}>
+                Iniciar sesión
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
