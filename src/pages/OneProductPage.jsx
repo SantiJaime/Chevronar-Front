f276cdcb783980a642e0def735726976d8e99af1
SantiJaime <santij03@gmail.com>
2024-03-03 20:11:22 -0300

    realizo manejo de errores en peticion de oneproduct y agrego spinner de carga

--- src/pages/OneProductPage.jsx
+++ src/pages/OneProductPage.jsx
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import clientAxios from "../utils/axiosClient";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Spinner } from "react-bootstrap";
 import Swal from "sweetalert2";
 
 const OneProductPage = () => {
   const params = useParams();
+  const navigate = useNavigate();
 
   const token = JSON.parse(sessionStorage.getItem("token"));
   const role = JSON.parse(sessionStorage.getItem("role"));
   const idUser = JSON.parse(sessionStorage.getItem("idUser"));
 
   const [prod, setProd] = useState({});
-  const [desc, setDesc] = useState([])
+  const [desc, setDesc] = useState([]);
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
+
   const getProduct = async () => {
-    const res = await clientAxios.get(`/productos/${params.id}`);
-    setProd(res.data.oneProd);
-    setDesc(res.data.oneProd.descripcion.split("| "))
+    try {
+      const res = await clientAxios.get(`/productos/${params.id}`);
+      setProd(res.data.oneProd);
+      setDesc(res.data.oneProd.descripcion.split("| "));
+      setMostrarSpinner(false);
+    } catch (error) {
+      navigate("/*");
+      Swal.fire({
+        icon: "error",
+        title: "¡Al parecer hubo un error",
+        text: error.response.data.msg[0].msg,
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    }
   };
 
   const addCart = async (idProd) => {
@@ -74,7 +89,6 @@ const OneProductPage = () => {
 
   useEffect(() => {
     getProduct();
-    // console.log(desc)
   }, []);
 
   const handleWhatsapp = () => {
@@ -87,68 +101,76 @@ const OneProductPage = () => {
 
   return (
     <Container className="my-5 text-white">
-      <Row>
-        <Col
-          lg={4}
-          md={12}
-          sm={12}
-          className="d-flex justify-content-center my-3"
-        >
-          <img
-            src={prod.imagen}
-            className="img-fluid rounded-4"
-            alt={prod.nombre}
-          />
-        </Col>
-        <Col lg={8} md={12} sm={12} className="my-3">
-          <h1>{prod.nombre}</h1>
+      {mostrarSpinner ? (
+        <div className="text-center my-5">
+          <Spinner />
+          <h5 className="mt-3">Cargando producto...</h5>
+        </div>
+      ) : (
+        <>
+          <Row>
+            <Col
+              lg={4}
+              md={12}
+              sm={12}
+              className="d-flex justify-content-center my-3"
+            >
+              <img
+                src={prod.imagen}
+                className="img-fluid rounded-4"
+                alt={prod.nombre}
+              />
+            </Col>
+            <Col lg={8} md={12} sm={12} className="my-3">
+              <h1>{prod.nombre}</h1>
+              <hr />
+              {token ? (
+                <div className="preciosClass">
+                  <h3>${prod.precio}</h3>
+                  <small>*Precios sujetos a cambios sin previo aviso</small>
+                </div>
+              ) : (
+                <Link className="linkFooter fs-5" to={"/login"}>
+                  Debes iniciar sesión para ver los precios
+                </Link>
+              )}
+              <hr />
+              <ul className="list-disc">
+                {desc.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+              <hr />
+              <div className="d-flex justify-content-between">
+                {role !== "admin" && (
+                  <Button
+                    variant="outline-light"
+                    className="fs-5"
+                    onClick={handleWhatsapp}
+                  >
+                    ¿Tienes dudas? <i className="bi bi-whatsapp"></i>
+                  </Button>
+                )}
+                {token && role === "user" && (
+                  <Button
+                    variant="outline-light"
+                    className="fs-5"
+                    onClick={() => addCart(prod._id)}
+                  >
+                    <i className="bi bi-cart-plus-fill"></i> Añadir al carrito
+                  </Button>
+                )}
+              </div>
+            </Col>
+          </Row>
           <hr />
-          {token ? (
-            <div className="preciosClass">
-              <h3>${prod.precio}</h3>
-              <small>*Precios sujetos a cambios sin previo aviso</small>
-            </div>
-          ) : (
-            <Link className="linkFooter fs-5" to={"/login"}>
-              Debes iniciar sesión para ver los precios
+          <div className="text-center">
+            <Link className="btn btn-light fs-5" to={"/products"}>
+              <i className="bi bi-arrow-left-circle"></i> Volver a productos
             </Link>
-          )}
-          <hr />
-          <ul className="list-disc">
-            {
-              desc.map((item, index) => 
-              <li key={index}>{item}</li>)
-            }
-          </ul>
-          <hr />
-          <div className="d-flex justify-content-between">
-            {role !== "admin" && (
-              <Button
-                variant="outline-light"
-                className="fs-5"
-                onClick={handleWhatsapp}
-              >
-                ¿Tienes dudas? <i className="bi bi-whatsapp"></i>
-              </Button>
-            )}
-            {token && role === "user" && (
-              <Button
-                variant="outline-light"
-                className="fs-5"
-                onClick={() => addCart(prod._id)}
-              >
-                <i className="bi bi-cart-plus-fill"></i> Añadir al carrito
-              </Button>
-            )}
           </div>
-        </Col>
-      </Row>
-      <hr />
-      <div className="text-center">
-        <Link className="btn btn-light fs-5" to={"/products"}>
-          <i className="bi bi-arrow-left-circle"></i> Volver a productos
-        </Link>
-      </div>
+        </>
+      )}
     </Container>
   );
 };
